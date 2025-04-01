fb75449ed30fd37124b65e11f8fe080be57733f8
JK-But-Cool <jkclumagui@gmail.com>
2025-04-01 10:24:28 -0700

    Create lab 1 thing.py

---
+++ lab 1 thing.py
@@ -0,0 +1,45 @@
+# Opening File Handles and reading data from files
+# You can get the following files with wget
+# sudo wget -O text-flie-matrix.txt https://raw.githubusercontent.com/jimTheSTEAMClown/Python-Class-STEAM-Clown/refs/heads/main/text-file-matrix.txt
+# sudo wget -O text-file-mail-very-short.txt https://raw.githubusercontent.com/jimTheSTEAMClown/Python-Class-STEAM-Clown/refs/heads/main/text-file-mail-very-short.txt
+print('''
+This Lab is about opening a file handle, and 
+printing the file handle.
+
+Hint: Check out W3Schools File Handling
+- https://www.w3schools.com/python/python_file_handling.asp
+      ''')
+# Challenge 1
+# Describe the parts of the file handle
+print('''
+Challenge # 1
+----------------------------------------------------
+Can you explain each part of the file handle? 
+
+What does the following parts of the file handle mean?
+1) _io.TextIOWrapper 
+2) name='matrix.txt' 
+3) mode='r' 
+4) encoding='UTF-8'>
+      
+Hint: https://www.w3schools.com/python/python_file_handling.asp
+---
+''')
+# -------------------------------------------------
+print('''Answer to Challenge 1
+1. A class that returns a decoded file as an object
+2. The name of the file being accessed
+3. The mode of the file, currently set to read only
+4. Unicode Transformation Format - 8-Bit, represents the encoding for the webpage file (how it is stored) 
+-------------------------------------------------''')
+# What does the following parts of the file handle mean?
+# 1) _io.TextIOWrapper 
+# 2) name='text-file-matrix.txt' 
+# 3) mode='r' 
+# 4) encoding='UTF-8'>
+# print('The parts of a file handle are:')
+# -----------------------------
+xfile = open('text-file-matrix.txt')
+print(xfile)
+print('''
+-------------------------------------------------''')
