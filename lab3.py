b4b56451b649852d7d9628d031a8c8cec94f3bd3
JK-But-Cool <jkclumagui@gmail.com>
2025-04-02 09:51:20 -0700

    Create lab3.py

---
+++ lab3.py
@@ -0,0 +1,28 @@
+# Opening File Handles and reading data from files
+print('''
+This Lab is about opening a file handle, and reading 
+the file, line by line using the file method: 
+- file_handle = open(file_name) method
+      ''')
+# Challenges
+print('''
+Challenge #1 & #2
+----------------------------------------------------
+#1 - Edit the code below to count each line, 
+     and print the total out at the end
+      
+#2 - Edit the code below to open the text-file-mail-short.txt file
+---
+''')
+# -------------------------------------------------
+print('''Answer to Challenges
+-------------------------------------------------''')
+# -----------------------------
+fhand = open('text-file-matrix.txt')
+john = 0
+for line in fhand:
+    john += 1
+    print(line.rstrip())
+print('Line Count:', john)
+print('''
+-------------------------------------------------''')
